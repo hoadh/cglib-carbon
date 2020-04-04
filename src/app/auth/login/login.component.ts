050a83291af657b3f799cb9df474e5b92f316aea
Hoa <danghuyhoa@gmail.com>
2020-04-04 08:43:50 +0700

    fix bug wrong background-color in landing-page after signin

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, AfterViewInit {
+export class LoginComponent implements OnInit, AfterViewInit, OnDestroy {
+	oldColor: string;
 
 	constructor(private elementRef: ElementRef) {
 	}
@@ -14,7 +15,12 @@ export class LoginComponent implements OnInit, AfterViewInit {
 	}
 
 	ngAfterViewInit() {
+		this.oldColor = this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor;
 		this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#f4f4f4';
 	}
 
+	ngOnDestroy(): void {
+		// reset body background-color
+		this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.oldColor;
+	}
 }
