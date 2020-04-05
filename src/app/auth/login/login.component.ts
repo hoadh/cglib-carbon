70e378e250277405bd6b5f2a680513a2c3de8ac4
Hoa <danghuyhoa@gmail.com>
2020-04-05 17:53:04 +0700

    show error notification when signin

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,8 @@ import { LOGIN_STATE } from './login-state.enum';
 import { AuthService } from '../../_core/services/auth.service';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService, Notification } from 'carbon-components-angular';
 
 @Component({
 	selector: 'app-login',
@@ -18,9 +20,16 @@ export class LoginComponent implements OnInit, AfterViewInit, OnDestroy {
 		password: new FormControl(),
 	});
 
-	constructor(private elementRef: ElementRef, private auth: AuthService, private router: Router) { }
+	showedNotification: Notification;
 
-	ngOnInit() { }
+	constructor(
+		private elementRef: ElementRef,
+		private auth: AuthService,
+		private router: Router,
+		private notificationService: NotificationService) {}
+
+	ngOnInit() {
+	}
 
 	ngAfterViewInit() {
 		this.oldColor = this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor;
@@ -34,7 +43,7 @@ export class LoginComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	doSignin() {
 		this.currentState = LOGIN_STATE.DOING;
-		const { username, password } = this.userForm.value;
+		const {username, password} = this.userForm.value;
 		this.requestSignin(username, password);
 	}
 
@@ -60,7 +69,37 @@ export class LoginComponent implements OnInit, AfterViewInit, OnDestroy {
 			}
 		}, (error) => {
 			this.currentState = LOGIN_STATE.ERROR;
-			console.log('LOGIN_STATE.ERROR', error);
+			this.handleRequestError(error);
+
 		});
+	}
+
+	handleRequestError(error: any) {
+		console.log('LOGIN_STATE.ERROR', error);
+		if (error instanceof HttpErrorResponse) {
+			if (error.status === 0) {
+				this.showNotification('error', 'Lỗi đăng nhập', 'Lỗi chưa xác định. Bạn vui lòng refresh trang web và thử đăng nhập lại.');
+			} else if (error.status === 401) {
+				this.showNotification('error', 'Lỗi đăng nhập', 'Thông tin đăng nhập không chính xác.');
+			}
+		}
+	}
+
+	showNotification(type: string, title: string, message: string) {
+		this.closeCurrentNotification();
+		this.showedNotification = this.notificationService.showNotification({
+			type: type,
+			title: title,
+			message: message,
+			target: '.notification-container',
+			showClose: true,
+			lowContrast: true,
+		});
+	}
+
+	closeCurrentNotification() {
+		if (this.showedNotification !== undefined) {
+			this.notificationService.close(this.showedNotification);
+		}
 	}
 }
