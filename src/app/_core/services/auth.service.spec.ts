1b34b240a4ce7f5ae9dd27a3213cc162d01d940e
Hoa <danghuyhoa@gmail.com>
2020-04-12 02:49:29 +0700

    Fix auth.service.spec.ts

--- src/app/_core/services/auth.service.spec.ts
+++ src/app/_core/services/auth.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => TestBed.configureTestingModule({
+		providers: [AuthService, HttpClient, HttpHandler]
+	}));
 
-  it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		const service: AuthService = TestBed.get(AuthService);
+		expect(service).toBeTruthy();
+	});
 });
